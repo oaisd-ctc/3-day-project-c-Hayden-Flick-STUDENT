82c53b94cb1f6c8fd87f361e7bcedd1a2ef4151a
Tyler-Kerkstra-Student <144162779+Tyler-Kerkstra-Student@users.noreply.github.com>
2024-01-24 19:17:47 +0000

    balanced damage calc and spawned entities

--- Entities/Entity.cs
+++ Entities/Entity.cs
@@ -66,7 +66,7 @@ public class Entity
     }
     public void atkP(Entity target)
     {
-        target.SetHP(target.GetHP()-((this.GetStr() * 5 + RollForDamage())/target.GetDef()));
+        target.SetHP(target.GetHP()-(((this.GetStr() + this.GetSpe()) * 5 + RollForDamage())/(target.GetDef() + target.GetSpe())));
         if(target.GetHP() <= 0)
         {
             target.SetHP(0);
@@ -75,8 +75,7 @@ public class Entity
     }
     public void atkM(Entity target)
     {
-        target.SetHP(target.GetHP()-((this.GetMgk() * 5 + RollForDamage())/target.GetMdef()));
-        if(target.GetHP() <= 0)
+        target.SetHP(target.GetHP()-(((this.GetStr() + this.GetSpe()) * 5 + RollForDamage())/(target.GetDef() + target.GetSpe())));
         {
             target.SetHP(0);
             target.alive = false;
--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Player player1 = new Player("Knight", 200, 5, 1, 4, 2, 2);
-        Enemy enemy1 = new Enemy("Goblin", 125, 3, 2, 1, 3, 4);
+        Player player1 = new Player("Knight", 175, 4, 2, 5, 3, 2);
+        Player player2 = new Player("Rogue", 125, 3, 3, 2, 3, 5);
+        Player player3 = new Player("Mage", 100, 1, 5, 1, 5, 2);
+        Enemy enemy1 = new Enemy("Goblin", 75, 3, 1, 3, 3, 4);
+        Enemy enemy2 = new Enemy("Orc", 125, 4, 3, 4, 2, 3);
+        Enemy enemy3 = new Enemy("Demon", 200, 4, 5, 5, 4, 1);
         player1.DisplayStats();
         enemy1.DisplayStats();
         player1.atkP(enemy1);
