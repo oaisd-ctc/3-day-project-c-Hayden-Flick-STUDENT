1afa4a561d2758117d4d5419ee4581fc7796709a
Tyler-Kerkstra-Student <144162779+Tyler-Kerkstra-Student@users.noreply.github.com>
2024-01-26 17:31:04 +0000

    rebalanced goblin

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ public class Program
     public static Player player1 = new Player("Knight", 175, 4, 2, 5, 3, 2);
     public static Player player2 = new Player("Rogue", 125, 3, 3, 2, 3, 5);
     public static Player player3 = new Player("Mage", 100, 1, 5, 1, 5, 2);
-    public static Enemy enemy1 = new Enemy("Goblin", 75, 3, 1, 3, 3, 4);
+    public static Enemy enemy1 = new Enemy("Goblin", 75, 3, 1, 1, 2, 4);
     public static Enemy enemy2 = new Enemy("Orc", 125, 4, 3, 4, 2, 3);
     public static Enemy enemy3 = new Enemy("Demon", 200, 4, 5, 5, 4, 1);
     public static Player[] arrayPlayer = { player1, player2, player3 };
